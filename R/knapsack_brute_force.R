c57d81545e5e8109170caf1321b96a063f47eb00
ketchup-boy <63000307+ketchup-boy@users.noreply.github.com>
2024-10-15 15:34:20 +0200

    finnished brute force and ongoing on dynamic programming

--- R/knapsack_brute_force.R
+++ R/knapsack_brute_force.R
@@ -1,21 +1,24 @@
 knapsack_brute_force <- function(x, W){
+  stopifnot("v" %in% names(x) & "w" %in% names(x) & all(x$v > 0) & all(x$w > 0))
   n <- length(x$v)
   current_max_weight <- 0
   current_best_items <- 0
+  current_best_value <- 0
   for(i in 1:((2^n)-1)){
     convertion <- intToBits(i)
     indexes <- which(convertion == 01)
     current_weight <- sum(x$w[indexes])
+    current_value <- sum(x$v[indexes])
     #browser()
-    if((current_weight <= W) & (current_weight > current_max_weight)){
-      current_max_weight <- current_weight
+    if((current_weight <= W) & (current_value > current_best_value)){
+      current_best_value <- current_value
       current_best_items <- indexes
     }
   }
-  return(data.frame(items = current_best_items, weight = current_max_weight))
+  return(list(value = current_best_value, elements = current_best_items))
 }
 
-#result <- knapsack_brute_force(knapsack_objects, 8033)
+result <- knapsack_brute_force(knapsack_objects, 8033)
 
 
 
--- R/knapsack_dynamic_programming.R
+++ R/knapsack_dynamic_programming.R
@@ -1,16 +1,20 @@
+
+
 knapsack_dynamic_programming <- function(x, W){
   n <- length(x$v)
-  m <- matrix(0, nrows = n+1, ncols = W+1)
-  
-  for(i in 2:(n+1)){
-    for(j in 2:(W+1)){
-      if(x$W[i] > j){
+  m <- matrix(0, nrow = (n+1), ncol = (W+1))
+  for(i in 2:(n)){
+    for(j in 2:(W)){
+      if(x$w[i] > j){
         m[i,j] <- m[i-1,j]
       }
       else{
-        m[i,j] <- max(m[i-1, j], m[i-1, j-w[i]] + v[i])
+        m[i,j] <- max(m[i-1, j], m[i-1, j-x$w[i]] + x$v[i])
       }
     }
   }
-  return(m[n+1, W+1])
+  return(m[n, W])
 }
+
+
+result <- knapsack_dynamic_programming(knapsack_objects, 3300)
