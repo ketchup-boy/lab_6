60c61a9f66b2b1e3124c01ded9efca7405488409
ketchup-boy <63000307+ketchup-boy@users.noreply.github.com>
2024-10-18 14:29:52 +0200

    struggling with everything

---
+++ R/brute_force_knapsack_general.R
@@ -0,0 +1,25 @@
+#' generlized Brute Force Knapsack Solver
+#'
+#' Solves the knapsack problem using a parallelized brute force approach or with non paralellalized brute force approach depending on inpur.
+#' @param x A data frame containing two columns: \code{v} (values) and \code{w} (weights) for each item.
+#' @param W A numeric value representing the maximum capacity (weight) of the knapsack.
+#'
+#' @return A list with the following components:
+#'   \item{value}{The maximum value that can be obtained.}
+#'   \item{elements}{The indices of the items selected to obtain the maximum value.}
+#'   \item{weight}{The total weight of the selected items.}
+#'   \item{parallel}{True if user wants to run brute force with parallel, false if user wants regular brute force}
+#'
+#' @import parallel
+#' @details
+#' This function evaluates calls brute_force_knapsack or brute_force_knapsack_optimized depending on boolean input for parameter parallel
+#'
+#' @export 
+brute_force_knapsack_general <- function(x, W, parallel){
+  if(parallel){
+    return(brute_force_knapsack_parallel(x, W))
+  }
+  else{
+    return(brute_force_knapsack(x, W))
+  }
+}
--- R/parallel_brute_force_knapsack.R
+++ R/parallel_brute_force_knapsack.R
@@ -19,8 +19,7 @@ library(parallel)
 #' performance, it evaluates chunks of combinations in parallel using the \code{parallel} package. 
 #' Each chunk of combinations is processed separately, and the best combination is selected.
 #'
-#' @export brute_force_knapsack_parallel
-
+#' @export
 brute_force_knapsack_parallel <- function(x, W) {
   stopifnot("v" %in% names(x) & "w" %in% names(x) & all(x$v > 0) & all(x$w > 0))
   n <- length(x$v)
@@ -58,32 +57,4 @@ brute_force_knapsack_parallel <- function(x, W) {
   return(overall_best)
 }
 
-
-#' generlized Brute Force Knapsack Solver
-#'
-#' Solves the knapsack problem using a parallelized brute force approach or with non paralellalized brute force approach depending on inpur.
-#' @param x A data frame containing two columns: \code{v} (values) and \code{w} (weights) for each item.
-#' @param W A numeric value representing the maximum capacity (weight) of the knapsack.
-#'
-#' @return A list with the following components:
-#'   \item{value}{The maximum value that can be obtained.}
-#'   \item{elements}{The indices of the items selected to obtain the maximum value.}
-#'   \item{weight}{The total weight of the selected items.}
-#'   \item{parallel}{True if user wants to run brute force with parallel, false if user wants regular brute force}
-#'
-#' @details
-#' This function evaluates calls brute_force_knapsack or brute_force_knapsack_optimized depending on boolean input for parameter parallel
-#'
-#' @export brute_force_knapsack_general
-
-brute_force_knapsack_general <- function(x, W, parallel){
-  if(parallel){
-    return(brute_force_knapsack_parallel(x, W))
-  }
-  else{
-    return(brute_force_knapsack(x, W))
-  }
-}
-
-result_parallel <- brute_force_knapsack_parallel(knapsack_objects, 3000)
 
