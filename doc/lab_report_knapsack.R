9684cee11dff46797b5cd005876ec9aab5d12cdc
Ana-S-Work <anastanojcic03@gmail.com>
2024-10-18 14:57:16 +0200

    awgvhjcrw

--- doc/lab_report_knapsack.R
+++ doc/lab_report_knapsack.R
@@ -33,7 +33,6 @@ print(result$value)
 print(result$elements)
 
 ## ----brute force opt----------------------------------------------------------
-
 result <- brute_force_knapsack_optimized(knapsack_objects[1:8,], W = 3500)
 print(result$max_value)
 print(result$selected_items)
@@ -55,44 +54,4 @@ print(result$elements)
 result <- brute_force_knapsack_general(knapsack_objects[1:8,], W = 3500, TRUE)
 print(result$max_value)
 print(result$selected_items)
-
-## ----brute force time---------------------------------------------------------
-#system.time({
- # result <- brute_force_knapsack(knapsack_objects[1:16,], W = 3500)
-#})
-
-## ----dynamic time-------------------------------------------------------------
-#system.time({
-#  result <- knapsack_dynamic_programming(knapsack_objects[1:500,], W = 3500)
-#})
-
-## ----setup brute force opt vs regular-----------------------------------------
-#system.time({
-#brute_force_knapsack(knapsack_objects[1:16,], 3300)
-#})
-
-#system.time({
-#brute_force_knapsack_optimized(knapsack_objects[1:16,], 3000)
-#})
-
-## ----dynamic opt vs regular---------------------------------------------------
-#system.time({
-#knapsack_dynamic_programming(knapsack_objects[1:600,], 3300)
-#})
-
-#system.time({
-#knapsack_dynamic_programming_optimized(knapsack_objects[1:600,], 3000)
-#})
-
-## ----prute force par vs regular-----------------------------------------------
-#system.time({
-# result <- brute_force_knapsack_general(knapsack_objects[1:17,], 3300, FALSE)
-#})
-
-#system.time({
-#result_parallel <- brute_force_knapsack_general(knapsack_objects[1:17,], 3000, TRUE)
-#})
-
-## ----library------------------------------------------------------------------
-library(lab6)
 
